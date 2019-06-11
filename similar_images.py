8fbeb1ee832192a3bfe099ede9817f2c0e9b9574
ajasecka <ajasecka@purdue.edu>
2019-06-11 14:22:08 -0500

    works, but very simple with recognizing differences

--- similar_images.py
+++ similar_images.py
@@ -1,33 +1,49 @@
 import cv2
 import numpy as np
+import pprint as pp
 from similar_images_functions import *
 
 '''
 checks if images are similiar
-does not use Machine Learning
+no machine learning, but would prefer to if there was enough data
 '''
 
 def main():
     #getting images and checking for similar dimensions
-    img1 = cv2.imread('C:/Users/Andrew/Desktop/images/same2.JPG')
-    img2 = cv2.imread('C:/Users/Andrew/Desktop/images/same1.JPG')
+    img1 = cv2.imread('C:/Users/Andrew/Desktop/images/same3.JPG')
+    img2 = cv2.imread('C:/Users/Andrew/Desktop/images/no2.JPG')
     if img1.shape != img2.shape:
         raise Exception('Image dimensions are not the same')
 
     #displaying images and dimensions
     cv2.imshow("first image", img1)
     cv2.imshow("second image", img2)
-    print(img1.shape)
+    print('image dimensions: {}'.format(img1.shape))
     cv2.waitKey(0)
 
     #converting images to numpy array
-    img1np = np.asarray(img1)
-    img2np = np.asarray(img2)
+    img1 = np.asarray(img1)
+    img2 = np.asarray(img2)
 
-    #function for testing
-    show_diff(img1np, img2np)
+    # calculates difference between pixels of images
+    diffa = img_sub(img1, img2)
 
+    # adding weights to pixels
 
+    # removing all values lower than 50 in difference image
+    diffa = np.where(diffa < 50, 0, diffa)
+    pix_difference = 1 - (np.sum(diffa) / (640 * 3 * 480 * 256))
+    print('percentage of similarities between colored image pixel values (after threshold): {}'.format(pix_difference))
+
+    #final decision
+    if pix_difference > .85:
+        print('Images are same')
+    else:
+        print('Images are different')
 
 if __name__ == '__main__':
-    main()
+    #main()
+    xx = 8
+    yy = 8
+    arr = [[int(max(abs(x - (xx / 2)), abs(y - (yy / 2)))) for x in range(xx)] for y in range(yy)]
+    pp.pprint(arr)
--- similar_images_functions.py
+++ similar_images_functions.py
@@ -2,28 +2,43 @@ import cv2
 import numpy as np
 from PIL import Image
 
-#creates an image of the difference between the two inputted images
-#useful for testing and visualizing differences in images
-#INPUTS: numpy array (of an image), numpy array (of an image)
-#OUTPUT: N/A
-def show_diff(img1, img2):
-    #calculates difference between pixels of images
-    diff = np.subtract(img1, img2)
-    diffa = np.absolute(diff)
-
-    #removing all values lower than 128 in difference image
-    diffa = np.where(diffa < 128, 0, diffa)
-    print('percentage of difference between colored image pixel values (after thresholding): {}'.format(1 - (np.sum(diffa) / (640 * 3 * 480 * 256))))
-
-
-    #showing colored difference image and saving
+# creates an image of the difference between the two inputted images
+# useful for testing and visualizing differences in images
+# INPUTS: numpy array (of difference image)
+# OUTPUT: numpy array (of grayscale difference image)
+def show_diff(diffa):
+    # showing colored difference image and saving
     img = Image.fromarray(diffa, 'RGB')
-    #img.show()
+    img.show()
+    #cv2.waitKey(0)
     img.save('C:/Users/Andrew/Desktop/fixed.JPG')
 
-    #converting difference image to grayscale
+    # converting difference image to grayscale
     new = cv2.imread('C:/Users/Andrew/Desktop/fixed.JPG')
     gray = cv2.cvtColor(new, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow('diff image from grayscale', gray)
-    #cv2.waitKey(0)
-    print('percentage of difference between grayscale image pixel values (after thresholding): {}'.format(1 - (np.sum(diffa) / (640 * 3 * 480 * 256))))
+
+    return gray
+
+# finds the absolute difference in an image
+# necessary because np.subtract is circular and cv2.subtract is cut off
+# there could be a subtract function from a library that does this, but haven't found one
+# this also could be done in one line, but  this is more descriptive
+# INPUTS: numpy array (of an image), numpy array (of an image)
+# OUTPUT: numpy array (of an image)
+def img_sub(img1, img2):
+    truth_array = np.less(img1, img2)
+    subbed12 = np.subtract(img1, img2)
+    subbed21 = np.subtract(img2, img1)
+    diffa = np.where(truth_array, subbed21, subbed12)
+
+    return diffa
+
+# weights difference image with higher weights on outside of image
+# need more testing to decide if this is necessary or not
+# INPUT: numpy array (of an image)
+# OUTPUT: numpy array (of an image)
+# not finished
+def weigh_diff_img(img):
+    pass
+    # arr = [[int(max(abs(x - (img.shape[0] / 2)), abs(y - (img.shape[1] / 2)))) for x in range(img.shape[0])] for y in range(img.shape[1])]
+    #return np.dot(arr, img)
