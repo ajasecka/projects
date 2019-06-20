d5935fcc240aad868b9922a9d8a963305b54a022
ajasecka <ajasecka@purdue.edu>
2019-06-20 18:40:28 -0500

    works, no optimizations

--- pipe_game_solver.py
+++ pipe_game_solver.py
@@ -5,111 +5,78 @@ import pprint as pp
 solves game in which you connect the dots with the same colors together
 '''
 
-# dictionary of different colors and their position on the board
-color_dict = {'G': [1, 0],
-              'Y': [0, 6],
-              'B': [6, 2],
-              'R': [6, 3],
-              'T': [1, 4],
-              'O': [8, 6]}
+# dictionary of different colors and their beginning and end positions on the board
+color_dict = {'G': [[0, 2], [3, 1]],
+              'Y': [[0, 4], [3, 3]],
+              'B': [[1, 2], [4, 2]],
+              'R': [[0, 0], [4, 1]],
+              'O': [[1, 4], [4, 3]]}
+
+color_list = list(color_dict.keys())
 
 # size of board
-b_size = 9
+b_size = 5
+
 
 def main():
     # initializing board size and location of dots on board
     board = [['_'] * b_size for _ in range(b_size)]
-    board[1][0] = 'G'
-    board[3][5] = 'G'
-    board[0][6] = 'Y'
-    board[2][5] = 'Y'
-    board[6][2] = 'B'
-    board[3][4] = 'B'
-    board[6][3] = 'R'
-    board[1][7] = 'R'
-    board[1][4] = 'T'
-    board[8][8] = 'T'
-    board[8][6] = 'O'
-    board[7][7] = 'O'
+    for color in color_list:
+        for pos in color_dict[color]:
+            board[pos[0]][pos[1]] = color
 
-    colors(board, ['G', 'Y', 'B', 'R', 'T', 'O'], 0)
+    # pp.pprint(board)
+    for x in board:
+        print(x)
+    print('\n\n')
+
+    board[color_dict[color_list[0]][0][0]][color_dict[color_list[0]][0][1]] = '_'
+    colors(board, color_dict[color_list[0]][0], 0)
 
     # currently only doing this for the 'G' dot
     # check_area(board, [1, 0], board[1][0])
 
-    pp.pprint(board)
+    # pp.pprint(board)
 
 # goes through the list of colors and calls check_area to connect them
-# need to keep track of last "seen" dot placed so if color path does not exist, the path before it can be redone***
-def colors(board, color_list, pos):
-    # getting color positions from dictionary
-    # makes code more readable
-    x = color_dict[color_list[pos]][0]
-    y = color_dict[color_list[pos]][1]
-
-    if check_area(board, [x, y], board[x][y]):
-        if pos < len(color_list):
-            colors(board, color_list, (pos + 1))
-        else:
-            print('WE MADE IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    else:
-        return
-
-
-
-
-
 # finds path from one colored dot to the same colored dot
 # can be optimized according to how the game works, but will do that later
-# should also be able to be optimized code-wise, but will do that later
-# returns True if color is placed, False if not (not implemented yet)
-def check_area(board, tile, label):
-    # initialization
-    seen = 'X'
-    board[tile[0]][tile[1]] = seen
+def colors(board, tile, color):
 
-    # checking if the next tile has the corresponding colored dot
-    if         ((tile[0] > 0) and (board[tile[0] - 1][tile[1]] == label)) \
-            or ((tile[0] < 8) and (board[tile[0] + 1][tile[1]] == label)) \
-            or ((tile[1] > 0) and (board[tile[0]][tile[1] - 1] == label)) \
-            or ((tile[1] < 8) and (board[tile[0]][tile[1] + 1] == label)):
-        board[tile[0]][tile[1]] = label
-        return True
+    # checking if the current tile is out of bounds
+    if (tile[0] < 0) or (tile[0] > (b_size - 1)) or (tile[1] < 0) or (tile[1] > (b_size - 1)):
+        return
 
-    # checks if in bounds
-    if tile[0] > 0:
-        # checks if empty tile
-        if board[tile[0] - 1][tile[1]] == '_':
-            check_area(board, [tile[0] - 1, tile[1]], label)
-            # if colored dot was reached, convert 'seen' dots to the initial colored dot
-            if board[tile[0] - 1][tile[1]] == label:
-                board[tile[0]][tile[1]] = label
-                return True
+    # getting color positions from dictionary
+    # makes code more readable
+    x1 = tile[0]
+    y1 = tile[1]
+    end = color_dict[color_list[color]][1]
 
-    # all if statements follow pattern above
-    if tile[0] < 8:
-        if board[tile[0] + 1][tile[1]] == '_':
-            check_area(board, [tile[0] + 1, tile[1]], label)
-            if board[tile[0] + 1][tile[1]] == label:
-                board[tile[0]][tile[1]] = label
-                return True
+    # if tile is the other colored dot
+    if [x1, y1] == end:
+        if color < len(color_list) - 1:
+            board[color_dict[color_list[color + 1]][0][0]][color_dict[color_list[color + 1]][0][1]] = '_'
+            colors(board, color_dict[color_list[color + 1]][0], color + 1)
+            board[color_dict[color_list[color + 1]][0][0]][color_dict[color_list[color + 1]][0][1]] = color_list[color + 1]
+            # something to do with here
+        else:
+            print('DONE! (HOPEFULLY)')
+            pp.pprint(board)
 
-    if tile[1] > 0:
-        if board[tile[0]][tile[1] - 1] == '_':
-            check_area(board, [tile[0], tile[1] - 1], label)
-            if board[tile[0]][tile[1] - 1] == label:
-                board[tile[0]][tile[1]] = label
-                return True
+    #fix this - can go back into the original square
+    elif board[tile[0]][tile[1]] == '_':
+        board[tile[0]][tile[1]] = color_list[color]
+        # print('up')
+        colors(board, [tile[0] - 1, tile[1]], color)
+        # print('down')
+        colors(board, [tile[0] + 1, tile[1]], color)
+        # print('left')
+        colors(board, [tile[0], tile[1] - 1], color)
+        # print('right')
+        colors(board, [tile[0], tile[1] + 1], color)
 
-    if tile[1] < 8:
-        if board[tile[0]][tile[1] + 1] == '_':
-            check_area(board, [tile[0], tile[1] + 1], label)
-            if board[tile[0]][tile[1] + 1] == label:
-                board[tile[0]][tile[1]] = label
-                return True
-
-    # if there is no place to go, reset
-    board[tile[0]][tile[1]] = '_'
+        board[tile[0]][tile[1]] = '_'
 
 if __name__ == '__main__':
     main()
