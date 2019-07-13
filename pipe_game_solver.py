27c449d279b1a253baca01d9178b31f30414ba93
ajasecka <ajasecka@purdue.edu>
2019-07-13 18:15:22 -0500

    no adjacent tiles op; works

--- pipe_game_solver.py
+++ pipe_game_solver.py
@@ -1,25 +1,27 @@
 import pprint as pp
+import time
+import sys
 
 '''
 solves game to connect the dots with the same colors together
-no optimizations
+optimizations: pipes cannot be placed next to themselves
 '''
 
 # dictionary of different colors and their beginning and end positions on the board
-color_dict = {'G': [[0, 2], [3, 1]],
-              'Y': [[0, 4], [3, 3]],
-              'B': [[1, 2], [4, 2]],
-              'R': [[0, 0], [4, 1]],
-              'O': [[1, 4], [4, 3]]}
+color_dict = {'G': [[1, 3], [4, 4]],
+              'Y': [[2, 1], [4, 1]],
+              'B': [[1, 1], [4, 5]],
+              'R': [[1, 0], [3, 5]],
+              'O': [[5, 0], [3, 1]]}
 
 # list of colors
 color_list = list(color_dict.keys())
 
 # size of board
-b_size = 5
+b_size = 6
 
-# flag
-win = False
+# time
+t0 = time.time()
 
 def main():
     # initializing board size and location of dots on board
@@ -34,60 +36,51 @@ def main():
 
     # calls function initially
     board[color_dict[color_list[0]][0][0]][color_dict[color_list[0]][0][1]] = '_'
-    colors(board, color_dict[color_list[0]][0], 0)
+    colors(board, color_dict[color_list[0]][0], 0, None)
 
-    if not win:
-        print('No possible solution')
+    print('No possible solution')
 
 
 # finds path from one colored dot to the same colored dot
 # can be optimized according to how the game works, but will do that later
-def colors(board, tile, color):
+def colors(board, tile, color, before):
 
-    # print(color_list[color])
-    # pp.pprint(board)
-    # print()
-
-    # for modifying flag
-    global win
+    # other dot that is to be connected to
+    end = color_dict[color_list[color]][1]
 
     # checking if the current tile is out of bounds
     if (tile[0] < 0) or (tile[0] > (b_size - 1)) or (tile[1] < 0) or (tile[1] > (b_size - 1)):
         return
 
     # optimizations
-    # if      (tile[0] - 1 > 0 and board[tile[0] - 1][tile[1]] == color_list[color]) or \
-    #         (tile[0] + 1 < (b_size - 1) and board[tile[0] + 1][tile[1]] == color_list[color]) or \
-    #         (tile[1] - 1 > 0 and board[tile[0]][tile[1] - 1] == color_list[color]) or \
-    #         (tile[1] + 1 < (b_size - 1) and board[tile[0]][tile[1] + 1] == color_list[color]):
-    #     return
-
-    # getting color positions from dictionary
-    # makes code more readable
-    x1 = tile[0]
-    y1 = tile[1]
-    end = color_dict[color_list[color]][1]
+    # checks if out of bounds, an adjacent tile is the same color, it was not the previous tile, and it is not the end tile
+    if      (tile[0] - 1 > 0 and board[tile[0] - 1][tile[1]] == color_list[color] and before != 'U' and [tile[0] - 1, tile[1]] != end) or \
+            (tile[0] + 1 < (b_size - 1) and board[tile[0] + 1][tile[1]] == color_list[color] and before != 'D' and [tile[0] + 1, tile[1]] != end) or \
+            (tile[1] - 1 > 0 and board[tile[0]][tile[1] - 1] == color_list[color] and before != 'L' and [tile[0], tile[1] - 1] != end) or \
+            (tile[1] + 1 < (b_size - 1) and board[tile[0]][tile[1] + 1] == color_list[color] and before != 'R' and [tile[0], tile[1] + 1] != end):
+        return
 
     # if tile is the other colored dot
-    if [x1, y1] == end:
+    if [tile[0], tile[1]] == end:
         if color < len(color_list) - 1:
             # goes to next color in the list
             board[color_dict[color_list[color + 1]][0][0]][color_dict[color_list[color + 1]][0][1]] = '_'
-            colors(board, color_dict[color_list[color + 1]][0], color + 1)
+            colors(board, color_dict[color_list[color + 1]][0], color + 1, None)
             board[color_dict[color_list[color + 1]][0][0]][color_dict[color_list[color + 1]][0][1]] = color_list[color + 1]
         # all colors have been connected
         else:
             print('DONE!')
+            print('time taken: {}'.format(time.time() - t0))
             pp.pprint(board)
-            win = True
+            sys.exit(0)
 
     # checks if square is empty
     elif board[tile[0]][tile[1]] == '_':
         board[tile[0]][tile[1]] = color_list[color]
-        colors(board, [tile[0] - 1, tile[1]], color)
-        colors(board, [tile[0] + 1, tile[1]], color)
-        colors(board, [tile[0], tile[1] - 1], color)
-        colors(board, [tile[0], tile[1] + 1], color)
+        colors(board, [tile[0] - 1, tile[1]], color, 'D')
+        colors(board, [tile[0] + 1, tile[1]], color, 'U')
+        colors(board, [tile[0], tile[1] - 1], color, 'R')
+        colors(board, [tile[0], tile[1] + 1], color, 'L')
 
         board[tile[0]][tile[1]] = '_'
 
